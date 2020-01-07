247fc63a5d3a0b7a08f5d3358d61a646ee12cec4
Yassir Khaldi <y.khaldi@ncr-maroc.com>
2020-01-07 15:48:23 +0100

    * Update bootstrap to v4 * 17. Event Binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,4 +6,15 @@ import {Component} from '@angular/core';
 })
 export class ServerComponent {
 
+  id = 1;
+  disabled = true;
+  btnText = 'Disabled with Property binding';
+
+  constructor() {
+    setTimeout(() => {
+      this.disabled = false;
+      this.btnText = 'Enabled with Property binding';
+    }, 2000);
+  }
+
 }
